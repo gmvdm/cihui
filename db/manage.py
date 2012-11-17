6a36784725b99a33687d6604c19a972c0fd9bc39
Geoff Wilson <gmwils@gmail.com>
2012-11-17 17:21:30 +0100

    Re-write DATABASE_URL for Heroku compat.

--- db/manage.py
+++ db/manage.py
@@ -6,4 +6,5 @@ import os
 
 if __name__ == '__main__':
     db_url = os.environ.get('DATABASE_URL', 'postgresql://localhost:5432/cihui')
+    db_url = db_url.replace('postgres:', 'postgresql:', 1)
     main(url=db_url, debug='False', repository='db')
