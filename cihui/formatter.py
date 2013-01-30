887314c9569b639141a83b5e600a69b0df33ac2b
Geoff Wilson <gmwils@gmail.com>
2013-01-30 22:13:03 +0100

    Add a csv formatter

--- cihui/formatter.py
+++ cihui/formatter.py
@@ -1,4 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 Geoff Wilson <gmwils@gmail.com>
 
 
 def format_description(desc):
     return '; '.join(desc)
+
+def format_word_as_csv(word):
+    description = ''
+    if len(word) > 2:
+        description = format_description(word[2])
+    return u'"%s","%s","%s"' % (word[0], word[1], description)
--- test/formatter_test.py
+++ test/formatter_test.py
@@ -16,3 +16,17 @@ class TestDescriptionFormatter(unittest.TestCase):
     def test_example_description(self):
         desc = [u'grassland', u'prairie', u'CL:\u7247[pian4]']
         self.assertEqual(u'grassland; prairie; CL:\u7247[pian4]', formatter.format_description(desc))
+
+
+class TestCSVWordFormatter(unittest.TestCase):
+    def test_basic_formatting(self):
+        word = [u'大', 'da', ['big']]
+        self.assertEqual(u'"大","da","big"', formatter.format_word_as_csv(word))
+
+    def test_word_without_description(self):
+        word = [u'大', 'da']
+        self.assertEqual(u'"大","da",""', formatter.format_word_as_csv(word))
+
+
+
+
