e37f8ab27c732f35cb690d11baf208056ead711e
Geoff Wilson <gmwils@gmail.com>
2013-03-23 15:46:46 +0100

    Basic structure for feed formatting

--- cihui/atom_formatter.py
+++ cihui/atom_formatter.py
@@ -5,7 +5,18 @@
 from xml.etree import ElementTree
 
 
-def format_atom():
+def format_atom(title=None, entries=[]):
     feed_elem = ElementTree.Element('feed', {'xmlns': 'http://www.w3.org/2005/Atom'})
+    if title:
+        title_elem = ElementTree.SubElement(feed_elem, 'title')
+        title_elem.text = title
+
+    for entry in entries:
+        entry_elem = ElementTree.SubElement(feed_elem, 'entry')
+        for k, v in entry.iteritems():
+            sub_elem = ElementTree.SubElement(entry_elem, k)
+            sub_elem.text = v
 
     return ElementTree.tostring(feed_elem)
+
+
--- test/atom_formatter_test.py
+++ test/atom_formatter_test.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2012 Geoff Wilson <gmwils@gmail.com>
 
 from cihui import atom_formatter
+from xml.etree import ElementTree
+
 import unittest
 
 
@@ -9,3 +11,27 @@ class TestAtomFormatter(unittest.TestCase):
     def test_empty_list(self):
         self.assertIn('http://www.w3.org/2005/Atom', atom_formatter.format_atom())
 
+    def test_set_title(self):
+        atom_xml = atom_formatter.format_atom(title='feed title')
+        root_elem = ElementTree.fromstring(atom_xml)
+        title_elem = root_elem.find('{http://www.w3.org/2005/Atom}title')
+
+        self.assertEqual('feed title', title_elem.text)
+
+    def test_include_single_item(self):
+        entry_list = [{'title': 'Title', 'link': '/link/', 'updated': '2013-03-14T00:00:00+01:00'}]
+
+        atom_xml = atom_formatter.format_atom(title='Feed Title', entries=entry_list)
+        root_elem = ElementTree.fromstring(atom_xml)
+        entry_elem = root_elem.find('{http://www.w3.org/2005/Atom}entry')
+
+        self.assertEqual('Title', entry_elem.find('{http://www.w3.org/2005/Atom}title').text)
+
+    def test_multiple_items(self):
+        entry_list = [{'title': 'First'}, {'title': 'Second'}]
+        atom_xml = atom_formatter.format_atom(entries=entry_list)
+
+        root_elem = ElementTree.fromstring(atom_xml)
+        entries = root_elem.findall('{http://www.w3.org/2005/Atom}entry')
+
+        self.assertEqual(2, len(entries))
