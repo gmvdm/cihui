675128289ba228924e8064559a66276d669b20d7
Geoff Wilson <gmwils@gmail.com>
2013-04-20 22:37:27 +0200

    Add support for gunicorn

--- Procfile
+++ Procfile
@@ -1,11 +1,12 @@
-web:      python app/app.py
-debug:    python app/app.py --debug
+web:      gunicorn -k tornado -b 0.0.0.0:$PORT -w 4 main
+tornado:  python main.py
+debug:    python main.py --debug
 
 downgrade: alembic downgrade -1
 migrate: alembic upgrade head
 stats: radon cc --min B -s cihui test
 
-pep8: find app cihui test -name '*.py' | xargs pep8
+pep8: find main.py cihui test -name '*.py' | xargs pep8
 
 test: py.test test
 cov: py.test --cov cihui --cov-report term-missing
---
+++ main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 Geoff Wilson <gmwils@gmail.com>
+
+import os
+import sys
+import tornado.ioloop
+
+from cihui import app, data
+
+
+db_url = os.environ.get('DATABASE_URL', 'postgresql://localhost:5432/cihui')
+port = int(os.environ.get("PORT", 5000))
+debugMode = False
+
+if len(sys.argv) > 1 and sys.argv[1] == '--debug':
+    debugMode = True
+
+application = app.CiHuiApplication(data.AccountData(db_url),
+                                   data.ListData(db_url),
+                                   os.environ.get('COOKIE_SECRET', None),
+                                   debug=debugMode
+                                   )
+
+
+if __name__ == "__main__":
+    application.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
